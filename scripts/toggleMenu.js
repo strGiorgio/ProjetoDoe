bf3550e212f5c1efcf9182bc22d9bbb25a8bb0f4
Giorgio Alefe <giorgioa2002@gmail.com>
2021-07-19 19:53:29 -0300

    Create a folder to save scripts

---
+++ scripts/toggleMenu.js
@@ -0,0 +1,7 @@
+const divLoginSignIn = document.querySelector('#wrapper-loginSignIn');
+const dropdownMenu = document.querySelector('.dropdown');
+
+function toggleMenus() {
+    divLoginSignIn.classList.toggle('inactive');
+    dropdownMenu.classList.toggle('inactive');
+};
