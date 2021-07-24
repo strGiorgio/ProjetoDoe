1941e3e837ece15b8b1a39cbdcb8d82b42180454
Giorgio Alefe <giorgioa2002@gmail.com>
2021-07-24 19:39:00 -0300

    Create a script to open login screen

---
+++ scripts/switchToLogin.js
@@ -0,0 +1,6 @@
+const sectionLogin = document.querySelector('.loginSignIn-screen');
+
+function OpenLogin() {
+    document.body.style.overflow = "hidden";
+    sectionLogin.classList.remove('disabled');
+}
