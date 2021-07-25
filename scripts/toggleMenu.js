3c7e851717a68e9902edf459cb6658efd5925474
Giorgio Alefe <giorgioa2002@gmail.com>
2021-07-25 19:22:21 -0300

    Create script to toggle menu dropdown and put user name inside dropdown

--- scripts/toggleMenu.js
+++ scripts/toggleMenu.js
@@ -1,7 +1,22 @@
 const divLoginSignIn = document.querySelector('#wrapper-loginSignIn');
 const dropdownMenu = document.querySelector('.dropdown');
 
-function toggleMenus() {
+const sectionSignIn_2 = document.querySelector('.loginSignIn-screen');
+const sectionLogin_2 = document.querySelector('.loginSignIn-screen');
+
+const form = document.querySelector('.containerForm.signIn');
+const userName = document.querySelector('#dropdownMenuButton1');
+const valueName = document.querySelector('.text-input.firstName');
+
+
+form.addEventListener('submit', submit);
+
+function submit() {
+    userName.innerHTML = `${valueName.value}`
     divLoginSignIn.classList.toggle('inactive');
     dropdownMenu.classList.toggle('inactive');
+
+    sectionSignIn_2.classList.add('disabled')
+    sectionLogin_2.classList.add('disabled')
+    document.body.style.overflow = "initial";
 };
